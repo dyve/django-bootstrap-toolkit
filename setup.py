4c1dfcc83122a1bbb319db85daad1b21e23c16c4
Dylan Verheul <dylan.verheul@gmail.com>
2012-01-18 10:16:11 +0100

    Fixed setup script, bumped to 1.0.1

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ long_desc = open(root_dir + '/README.md').read()
 
 setup(
     name='django-bootstrap-toolkit',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/dyve/django-bootstrap-toolkit',
     author='Dylan Verheul',
     author_email='dylan@dyve.net',
     license='Apache License 2.0',
     install_requires=['Django'],
-    packages=['bootstrap', 'bootstrap.templatetags'],
-    package_data={'bootstrap': ['templates/bootstrap/*.html']},
+    packages=['bootstrap_toolkit', 'bootstrap_toolkit.templatetags'],
+    package_data={'bootstrap_toolkit': ['templates/bootstrap/*.html']},
     description='Bootstrap support for Django projects',
     classifiers=[
         "Development Status :: 4 - Beta",
