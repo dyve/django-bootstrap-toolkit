8ae89bb1d1c274233900514a55e07f97014f3fb6
Dylan Verheul <dylan.verheul@gmail.com>
2012-01-20 10:54:14 +0100

    Fixed file name, removed Django install req

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='django-bootstrap-toolkit',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/dyve/django-bootstrap-toolkit',
     author='Dylan Verheul',
     author_email='dylan@dyve.net',
     license='Apache License 2.0',
-    install_requires=['Django'],
+#   install_requires=['Django'],
     packages=['bootstrap_toolkit', 'bootstrap_toolkit.templatetags'],
     include_package_data=True,
     description='Bootstrap support for Django projects',
