eadcf2f9f6d3a7c0b5b79c42a89d2fd6da73c3ad
Dylan Verheul <dylan.verheul@gmail.com>
2012-03-04 14:52:11 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bootstrap-toolkit',
-    version='2.2.1',
+    version='2.3.1',
     url='https://github.com/dyve/django-bootstrap-toolkit',
     author='Dylan Verheul',
     author_email='dylan@dyve.net',
