27c1a38d95c9339f76459df7501c5b8518a12370
Dylan Verheul <dylan.verheul@gmail.com>
2012-03-19 07:35:54 +0100

    Added markup to vertical forms (standard Bootstrap) works fine, gives more control

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bootstrap-toolkit',
-    version='2.3.7',
+    version='2.4.0',
     url='https://github.com/dyve/django-bootstrap-toolkit',
     author='Dylan Verheul',
     author_email='dylan@dyve.net',
