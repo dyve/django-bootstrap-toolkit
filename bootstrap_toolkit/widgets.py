ef97b3ae82a41a68ca83541f5feef383edaf865d
Vitor Hirota <vitor.hirota@gmail.com>
2012-09-10 14:20:17 -0300

    Added BootstrapInput base widget to allow append/prepend, BootstrapTimeInput and BootstrapDateTimeInput

--- bootstrap_toolkit/widgets.py
+++ bootstrap_toolkit/widgets.py
@@ -2,6 +2,7 @@ from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
+
 def add_to_css_class(classes, new_class):
     new_class = new_class.strip()
     if new_class:
@@ -18,11 +19,65 @@ def add_to_css_class(classes, new_class):
         classes = u" ".join(classes)
     return classes
 
-class BootstrapDateInput(forms.TextInput):
+
+class BootstrapInput(forms.TextInput):
 
     bootstrap = {
-        'append': mark_safe('<i class="icon-th"></i>'),
-        'prepend': None,
+                 'append': None,
+                 'prepend': None
+                 }
+    attrs = {}
+
+    def __init__(self, attrs=None, bootstrap_attrs=None):
+        if bootstrap_attrs is not None:
+            self.bootstrap.update(bootstrap_attrs)
+        if attrs is not None:
+            self.attrs = self.build_attrs(attrs)
+        super(BootstrapInput, self).__init__(self.attrs)
+
+        print 'BootstrapInput.__init__', self.bootstrap, self.attrs
+
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        attrs = dict(self.attrs, **kwargs)
+        if extra_attrs:
+            if 'class' in extra_attrs and 'class' in attrs:
+                attrs['class'] = add_to_css_class(attrs['class'],
+                                                  extra_attrs.pop('class'))
+            attrs.update(extra_attrs)
+        return attrs
+
+    def render(self, name, value, attrs=None):
+        css_class = []
+        prepend = ''
+        append = ''
+        final_attrs = self.build_attrs(attrs)
+        if self.bootstrap['prepend']:
+            css_class.append('input-prepend')
+            prepend = '<span class="add-on">%s</span>' % (
+                self.bootstrap['prepend']
+            )
+        if self.bootstrap['append']:
+            css_class.append('input-append')
+            append = '<span class="add-on">%s</span>' % (
+                self.bootstrap['append']
+            )
+
+        return mark_safe('<div class="%s">%s%s%s</div>' % (
+            u' '.join(css_class),
+            prepend,
+            super(BootstrapInput, self).render(name, value, final_attrs),
+            append,
+        ))
+
+
+class BootstrapDateInput(BootstrapInput, forms.DateInput):
+
+    bootstrap = {
+        'append': None,
+        'prepend': mark_safe('<i class="icon-calendar"></i>'),
+    }
+    attrs = {
+        'class': 'datepicker'
     }
 
     class Media:
@@ -36,11 +91,36 @@ class BootstrapDateInput(forms.TextInput):
             )
         }
 
-    def render(self, name, value, attrs=None):
-        if not attrs:
-            attrs = {}
-        if 'class' in attrs:
-            attrs['class'] = add_to_css_class(attrs['class'], 'datepicker')
-        else:
-            attrs['class'] = 'datepicker'
-        return super(BootstrapDateInput, self).render(name, value, attrs)
+
+class BootstrapTimeInput(BootstrapInput, forms.TimeInput):
+
+    bootstrap = {
+        'append': None,
+        'prepend': mark_safe('<i class="icon-time"></i>')
+    }
+    attrs = {
+        'class': 'timepicker'
+    }
+
+    class Media:
+        js = (
+            settings.STATIC_URL + 'timepicker/js/bootstrap-timepicker.js',
+            settings.STATIC_URL + 'bootstrap_toolkit/js/init_timepicker.js',
+        )
+        css = {
+            'screen': (
+                settings.STATIC_URL + 'timepicker/css/timepicker.css',
+            )
+        }
+
+
+class BootstrapDateTimeInput(forms.SplitDateTimeWidget):
+
+    def __init__(self, attrs=None):
+        widgets = [
+                   BootstrapDateInput(attrs={'class': 'input-small',
+                                             'style': 'margin-right: 15px;'}),
+                   BootstrapTimeInput(attrs={'class': 'input-mini'})]
+        # Note that we're calling MultiWidget, not SplitDateTimeWidget, because
+        # we want to define widgets.
+        forms.MultiWidget.__init__(self, widgets, attrs)
