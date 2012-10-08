a07004c322e9f11b41a0d03610f28a08af027dea
Dylan Verheul <dylan@zostera.nl>
2012-10-08 10:55:35 +0200

    version++

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bootstrap-toolkit',
-    version='2.5.8',
+    version='2.5.9',
     url='https://github.com/dyve/django-bootstrap-toolkit',
     author='Dylan Verheul',
     author_email='dylan@dyve.net',
