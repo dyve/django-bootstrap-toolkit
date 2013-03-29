fba27df92d2565ff5d716e20af5405870bb6d67b
Dylan Verheul <dylan@zostera.nl>
2013-03-29 09:58:38 +0100

    Django 1.5 fixes

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,12 +16,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    (r'^$', direct_to_template, {'template': 'index.html'}, "home"),
-    (r'^contact$', direct_to_template, {'template': 'contact.html'}, "contact"),
-    (r'^form$', 'demo_app.views.demo_form'),
-    (r'^form_template$', 'demo_app.views.demo_form_with_template'),
-    (r'^form_inline$', 'demo_app.views.demo_form_inline'),
-    (r'^tabs$', 'demo_app.views.demo_tabs', {}, "tabs"),
-    (r'^pagination$', 'demo_app.views.demo_pagination', {}, "pagination"),
-    (r'^widgets$', 'demo_app.views.demo_widgets', {}, "widgets"),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name="contact"),
+    url(r'^form$', 'demo_app.views.demo_form'),
+    url(r'^form_template$', 'demo_app.views.demo_form_with_template'),
+    url(r'^form_inline$', 'demo_app.views.demo_form_inline'),
+    url(r'^tabs$', 'demo_app.views.demo_tabs', {}, "tabs"),
+    url(r'^pagination$', 'demo_app.views.demo_pagination', {}, "pagination"),
+    url(r'^widgets$', 'demo_app.views.demo_widgets', {}, "widgets"),
 )
