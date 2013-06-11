cf1927759b80f77eebdf5942cd4c3c375794b30b
Dylan Verheul <dylan@zostera.nl>
2013-06-11 16:43:21 +0200

    Bumped CDN Bootstrap version to 2.3.2

--- bootstrap_toolkit/templatetags/bootstrap_toolkit.py
+++ bootstrap_toolkit/templatetags/bootstrap_toolkit.py
@@ -14,7 +14,7 @@ from django.utils.html import escape
 
 
 BOOTSTRAP_BASE_URL = getattr(settings, 'BOOTSTRAP_BASE_URL',
-                             '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/'
+                             '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/'
 )
 
 BOOTSTRAP_JS_BASE_URL = getattr(settings, 'BOOTSTRAP_JS_BASE_URL',
